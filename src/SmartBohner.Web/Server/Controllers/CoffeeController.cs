fe01c5ce86f976de2e3654300b23018375861eaa
lwnwim8 <michael.widerin@liebherr.com>
2021-12-02 11:37:01 +0100

    added swagger documentation

---
+++ src/SmartBohner.Web/Server/Controllers/CoffeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBohner.Web.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        [HttpGet]
+        public bool Test()
+        {
+            return true;
+        }
+    }
+}
