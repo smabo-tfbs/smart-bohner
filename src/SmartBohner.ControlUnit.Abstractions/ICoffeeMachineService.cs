5f1ac264d147a6ff9fce2816e0d5b32071b649fa
Felix Eckl <felixeckl@outlook.com>
2021-12-02 11:51:06 +0100

    code review, added images with pinout

--- src/SmartBohner.ControlUnit.Abstractions/ICoffeeMachineService.cs
+++ src/SmartBohner.ControlUnit.Abstractions/ICoffeeMachineService.cs
@@ -21,12 +21,12 @@ namespace SmartBohner.ControlUnit.Abstractions
         /// Send a signal to the coffee-machine to start up
         /// </summary>
         /// <returns></returns>
-        Task<bool> Start();
+        Task Start();
 
         /// <summary>
         /// Send a signal to the coffee-machine to shutdown
         /// </summary>
         /// <returns></returns>
-        Task<bool> Shutdown();
+        Task Shutdown();
     }
 }
