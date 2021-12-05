819aa41182a794d034b80444c9c68b521bca7df5
Felix Eckl <felixeckl@outlook.com>
2021-12-05 23:43:43 +0100

    Added controller for ci testing :)

---
+++ src/SmartBohner.Web/Server/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBohner.Web.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return "HelloDocker!!!";
+        }
+    }
+}
