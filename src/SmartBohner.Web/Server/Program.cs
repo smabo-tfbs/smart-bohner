dc647b104a5ec1bf421b10dc4c4a7c25001624e5
Felix Eckl <felixeckl@outlook.com>
2021-12-09 22:50:49 +0100

    Added logging to seq to make troubleshooting easier

--- src/SmartBohner.Web/Server/Program.cs
+++ src/SmartBohner.Web/Server/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using SmartBohner.ControlUnit.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +13,21 @@ builder.Services.AddSwaggerDocument(c =>
     c.Description = "Communicate with smart coffee machine.";
 });
 
+LoggerConfiguration configuration = new LoggerConfiguration();
+configuration
+    .Enrich.WithProperty("Application", "smabo-webapi")
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq("http://it01.refame.net:5341", apiKey: "WYCYRdPTNVREN0eoILFa");
+
+builder.WebHost.UseSerilog(configuration.CreateLogger());
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
