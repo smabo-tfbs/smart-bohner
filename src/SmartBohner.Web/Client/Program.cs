696826e491abf722f5266d3afff5d21842bdf2c5
lwnwim8 <michael.widerin@liebherr.com>
2022-01-13 16:32:04 +0100

    added home screen with child components

--- src/SmartBohner.Web/Client/Program.cs
+++ src/SmartBohner.Web/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartBohner.Web.Client;
+using SmartBohner.Web.Client.Infrastructure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
