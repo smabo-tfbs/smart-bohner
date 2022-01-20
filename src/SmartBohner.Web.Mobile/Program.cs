5aa964ea7cc52da9b713b6f7e46bfacfc678481a
Felix <60475215+FAMEEXE@users.noreply.github.com>
2022-01-20 16:10:42 +0100

    Added smart-bohner UI (#40)
    * added basic raspberry ui mockup
    
    * update
    
    * added home screen with child components
    
    * finished frontend without signalr
    
    * added svgs
    
    * Merge UI into UI2 (#39)
    
    * Added Project for mobile and Web UI
    
    * Added Banner and Buttons
    worked on Index Page
    
    Co-authored-by: Warbarbie <77971761+Warbarbie@users.noreply.github.com>
    
    Co-authored-by: lwnwim8 <michael.widerin@liebherr.com>
    Co-authored-by: Warbarbie <77971761+Warbarbie@users.noreply.github.com>

--- src/SmartBohner.Web.Mobile/Program.cs
+++ src/SmartBohner.Web.Mobile/Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SmartBohner.Web.Client;
-using SmartBohner.Web.Client.Infrastructure;
+using SmartBohner.Web.Mobile;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
---
+++ src/SmartBohner.Web/Client/Infrastructure/CoffeeType.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartBohner.Web.Client.Infrastructure
+{
+    public enum CoffeeType
+    {
+        Coffee,
+        Espresso,
+        Lungo
+    }
+}
