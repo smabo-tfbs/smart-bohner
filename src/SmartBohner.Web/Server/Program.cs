52844e51967a4806898234e32f24c3b52edf0801
Michael <63611191+michaelwiderin@users.noreply.github.com>
2022-01-22 11:46:50 +0100

    configured publish pipepline and application mapping (#41)

--- src/SmartBohner.Web/Server/Program.cs
+++ src/SmartBohner.Web/Server/Program.cs
@@ -1,9 +1,17 @@
+using Namotion.Reflection;
 using Serilog;
 using Serilog.Formatting.Json;
 using SmartBohner.ControlUnit.AspNet;
 using SmartBohner.ControlUnit.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure kestrel to listen on port 5001 (Raspberry) and 5002 (Desktop, Mobile, ...)
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5001);
+    options.ListenAnyIP(5002);
+});
 
 // Add services to the container.
 builder.Services.RegisterControlUnit();
@@ -21,11 +29,11 @@ configuration
     .WriteTo.Console()
     .WriteTo.Debug()
     .WriteTo.File(
-            new JsonFormatter(), 
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "smabo", "logs", "log.json"), 
-            rollingInterval: RollingInterval.Day, 
-            rollOnFileSizeLimit: true, 
-            fileSizeLimitBytes: 1024 * 1024 * 1024, 
+            new JsonFormatter(),
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "smabo", "logs", "log.json"),
+            rollingInterval: RollingInterval.Day,
+            rollOnFileSizeLimit: true,
+            fileSizeLimitBytes: 1024 * 1024 * 1024,
             shared: true);
 
 builder.WebHost.UseSerilog(configuration.CreateLogger());
@@ -48,18 +56,51 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
 app.UseOpenApi();
 app.UseSwaggerUi3();
 app.UseSerilogRequestLogging();
 
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.UseWhen(context => context.Request.Host.Port == 5001, config =>
+{
+    config.Use((context, next) =>
+    {
+        context.Request.Path = "/raspy" + context.Request.Path;
+        return next();
+    });
+
+    config.UseStaticFiles();
+    config.UseBlazorFrameworkFiles("/raspy");
+    config.UseStaticFiles("/raspy");
+    config.UseRouting();
+    
+    config.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+        endpoints.MapFallbackToFile("/raspy/{*path:nonfile}",
+            "raspy/index.html");
+    });
+});
+
+app.UseWhen(context => context.Request.Host.Port == 5002, config =>
+{
+    config.Use((context, next) =>
+    {
+        context.Request.Path = "/mobile" + context.Request.Path;
+        return next();
+    });
+
+    config.UseStaticFiles();
+    config.UseBlazorFrameworkFiles("/mobile");
+    config.UseStaticFiles("/mobile");
+    config.UseRouting();
+
+    config.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+        endpoints.MapFallbackToFile("/mobile/{*path:nonfile}",
+            "mobile/index.html");
+    });
+});
 
 app.InitControlUnit();
 app.Run();
