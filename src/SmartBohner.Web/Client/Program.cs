821437aab71df74045762f514c3ecace6e329c12
michaelwiderin <michael.widerin@hotmail.com>
2022-01-22 18:39:57 +0100

    added mock service for signalr connection

--- src/SmartBohner.Web/Client/Program.cs
+++ src/SmartBohner.Web/Client/Program.cs
@@ -2,12 +2,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartBohner.Web.Client;
 using SmartBohner.Web.Client.Infrastructure;
+using SmartBohner.Web.Client.Infrastructure.Mock;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<IWarningHubConnectionHandler, WarningHubConnectionHandler>();
+
+var mockWarningHandler = builder.Configuration.GetValue<bool>("MockWarningHandler");
+if (mockWarningHandler)
+    builder.Services.AddSingleton<IWarningHubConnectionHandler, MockWarningHubConnectionHandler>();
+else
+    builder.Services.AddSingleton<IWarningHubConnectionHandler, WarningHubConnectionHandler>();
 
 await builder.Build().RunAsync();
