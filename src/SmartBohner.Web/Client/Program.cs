e612293440e5b573a3d0c624ee23fb99a707a0e5
michaelwiderin <michael.widerin@hotmail.com>
2022-01-22 19:23:02 +0100

    added resource file for warning messages

--- src/SmartBohner.Web/Client/Program.cs
+++ src/SmartBohner.Web/Client/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization();
 
 var mockWarningHandler = builder.Configuration.GetValue<bool>("MockWarningHandler");
 if (mockWarningHandler)
