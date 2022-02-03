5d0b3009071f5518a59940dfab9e357b7a2ff2f3
Warbarbie <77971761+Warbarbie@users.noreply.github.com>
2022-02-03 09:56:58 +0100

    Feature/webui Indexpage/Button and Banner (#43)
    * Update Banner and Buttons
    
    * Update
    
    * Updatetd Indexpage and changed Background, slight Button and Banner ajustment
    
    * Changed 2 GetAsync
    
    * Added testing routine to action
    
    * Restructured Gpio (#44)
    
    * Added pinout to .md
    
    * Added functionality to services (#45)
    
    * Connected the Client to the Web-Api
    
    * Added further implementation
    
    * SignalR in production, Added test controller
    
    * Added logging
    
    * Resolved DI-Error
    
    * The pinservicefactory gets used, not the pinservice it self
    
    * Added pins to factory
    
    * implemented warnings, moved signalr handler and resource file to shared (#46)
    
    * Rewrote WebUI Design and added Buttons
    
    * Update Banner and Buttons
    
    * Update
    
    * Updatetd Indexpage and changed Background, slight Button and Banner ajustment
    
    * Changed 2 GetAsync
    
    * Rewrote WebUI Design and added Buttons
    
    * Added BannerItem
    
    * changed Button width
    
    Co-authored-by: Felix Eckl <felixeckl@outlook.com>
    Co-authored-by: Felix <60475215+FAMEEXE@users.noreply.github.com>
    Co-authored-by: Michael <63611191+michaelwiderin@users.noreply.github.com>

--- src/SmartBohner.Web.Mobile/Program.cs
+++ src/SmartBohner.Web.Mobile/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartBohner.Web.Mobile;
+using SmartBohner.Web.Shared.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress.Replace("mobile", null)) });
+builder.Services.RegisterShared();
 
 await builder.Build().RunAsync();
